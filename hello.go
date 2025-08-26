68ac072edc1a44271ffc1e1f1c58ff9469050117
BecomingEngineer <nazarurban2007@gmail.com>
2025-08-26 18:28:05 +0300

    ÐAll with a little

--- hello.go
+++ hello.go
@@ -1,5 +1,5 @@
 package main
 
 func Hello() string {
-	return "Hello"
+	return "hello"
 }
--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ func TestHello(t *testing.T) {
 		name string
 		want string
 	}{
-		{"basic", "Hello"}}
+		{"basic", "hello"}}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
